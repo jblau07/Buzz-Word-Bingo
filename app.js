490cd339e397382d5fa17a333b5a0666a44011a7
jblau07 <jlau7248@gmail.com>
2018-01-31 10:41:37 -1000

    get, post, put, delete work

--- app.js
+++ app.js
@@ -2,12 +2,16 @@ const express = require('express');
 const app = express();
 const buzzRoutes = require('./routes/buzzwords');
 
+
 const PORT = process.env.PORT || 8080;
 const bodyParse = require('body-parser');
+
 
 app.use(express.static('public'));
 
 app.use(bodyParse.urlencoded({ extended: true }))
+
+app.use('/buzzwords', buzzRoutes);
 
 app.listen(8080, (err) => {
   if (err) {throw err;}
--- routes/buzzwords.js
+++ routes/buzzwords.js
@@ -1,6 +1,56 @@
 const express = require('express');
 const router = express.Router();
 
+const collection = [];
 
+router.get('/', (req, res) => {
+    if (collection.length > 0) {
+      res.json({
+        buzzWords: collection
+      })
+    } else {
+      res.json({
+        error: 'the player has no words stored'
+      });
+    }
+  })
+  .post('/', (req, res) => {
+    if (collection.length < 5) {
+      collection.push(req.body);
+      res.json({
+        success: true
+      })
+      console.log(collection)
+    } else {
+      res.json({
+        error: 'player cannot store more than 5 words'
+      });
+    }
+  })
+  .put('/', (req, res) => {
+    let index = collection.findIndex(element => element.buzzWord === req.body.buzzWord);
+    console.log(index);
+
+    if (index >= 0) {
+      collection[index].points = req.body.points;
+      console.log(collection);
+      res.json({
+        success: true
+      })
+    } else {
+      res.json({
+        error: 'selected buzzword does not exist'
+      })
+    }
+  })
+  .delete('/', (req, res) => {
+    let index = collection.findIndex(element => element.buzzWord === req.body.buzzWord);
+    if (index >= 0) {
+      collection.splice(index, 1);
+      res.json({success: true})
+    } else {
+      res.json({error: 'selected buzzWord is not in the players stored words'})
+    }
+  })
 
 module.exports = router;
